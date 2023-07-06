21f2c739cea1668750342b84b2b529294c433d01
JaeSeong Song <zzyzigy@naver.com>
2023-07-06 15:33:35 +0900

    회원가입 : agree 처리 추가

---
+++ src/main/resources/static/js/project.js
@@ -0,0 +1,25 @@
+// agree
+let agree1 = document.querySelector("#agree1");
+let agree2 = document.querySelector("#agree2");
+let okagree = document.querySelector("#okagree");
+let noagree = document.querySelector("#noagree");
+
+okagree?.addEventListener('click', () => {
+    if (!agree1.checked) alert('이용약관 동의에 체크하세요!');
+    else if (!agree2.checked) alert('개인정보 이용 동의에 체크하세요!');
+    else location.href = '/join/checkme';
+});
+noagree?.addEventListener('click', () => {
+    location.href = "/";
+});
+
+
+
+
+
+
+
+
+
+
+
