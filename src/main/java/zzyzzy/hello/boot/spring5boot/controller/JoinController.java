1ba31d0b7bd1692e9e1e88e8724b6d90341806f0
JaeSeong Song <zzyzigy@naver.com>
2023-07-06 15:51:04 +0900

    회원가입 : 실명 확인 처리

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/JoinController.java
@@ -4,7 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zzyzzy.hello.boot.spring5boot.model.Checkme;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/join")
@@ -24,6 +28,21 @@ public class JoinController {
         logger.info("join/checkme 호출!!");
 
         return "join/checkme";
+    }
+
+    @PostMapping("/checkme")
+    public String checkmeok(Checkme checkme, HttpSession sess) {
+        logger.info("join/checkmeok 호출!!");
+        // checkme에 보낸 개인정보를 세션에 저장하고
+        // joinme로 이동
+        String viewPage = "redirect:/join/checkme";
+
+        if (checkme.getName() != null) {
+            sess.setAttribute("checkme", checkme);
+            viewPage = "redirect:/join/joinme";
+        }
+
+        return viewPage;
     }
 
     @GetMapping("/joinme")
