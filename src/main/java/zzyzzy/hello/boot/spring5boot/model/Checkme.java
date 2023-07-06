de89e9635b438e940ae375432a997702cf0736c5
JaeSeong Song <zzyzigy@naver.com>
2023-07-06 15:50:41 +0900

    회원가입 : 실명 확인 정보용 클래스

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/Checkme.java
@@ -0,0 +1,14 @@
+package zzyzzy.hello.boot.spring5boot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Checkme {
+    private String name;
+    private String jumin1;
+    private String jumin2;
+}
