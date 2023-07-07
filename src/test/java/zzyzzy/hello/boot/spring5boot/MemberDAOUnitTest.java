75d4ebef9292b3f7584f89bf21d5a0a566c91ee0
JaeSeong Song <zzyzigy@naver.com>
2023-07-07 11:56:46 +0900

    회원가입 : MemberDAO 단위테스트 (성공!)

--- src/test/java/zzyzzy/hello/boot/spring5boot/MemberDAOUnitTest.java
+++ src/test/java/zzyzzy/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -2,26 +2,24 @@ package zzyzzy.hello.boot.spring5boot;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import zzyzzy.hello.boot.spring5boot.dao.MemberDAO;
 import zzyzzy.hello.boot.spring5boot.dao.MemberDAOImpl;
 import zzyzzy.hello.boot.spring5boot.model.Member;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(MemberDAOImpl.class)
 public class MemberDAOUnitTest {
 
-    @Autowired
-    private MemberDAO memberDAOImpl;
+    @Autowired private MemberDAO mdao;
 
     @Test
     @DisplayName("MemberDAO insert Test")
@@ -29,11 +27,10 @@ public class MemberDAOUnitTest {
         Member m = new Member(null,"","","",
             "","","","","",null);
 
-        int result = memberDAOImpl.insertMember(m);
+        int result = mdao.insertMember(m);
         System.out.println(result);
         assertEquals(result, 1);
     }
-
 
 }
 
