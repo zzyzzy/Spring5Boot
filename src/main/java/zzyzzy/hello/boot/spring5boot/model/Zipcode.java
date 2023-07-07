ae65c5b03a023238f9fc4002b6040bc2aa179684
JaeSeong Song <zzyzigy@naver.com>
2023-07-07 16:22:14 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,17 @@
+package zzyzzy.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Zipcode {
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+}
