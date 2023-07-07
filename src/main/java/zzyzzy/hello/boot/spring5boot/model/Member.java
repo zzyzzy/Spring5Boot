ab42b8d4cf5431862db9b7a602b5a0f52ac2319c
JaeSeong Song <zzyzigy@naver.com>
2023-07-07 10:38:54 +0900

    회원가입 : Member 클래스 추가

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,27 @@
+package zzyzzy.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdate;
+}
+
+
+
+
+
+
