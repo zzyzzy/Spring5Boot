a6acf6d8fee590ffe8f4befef7d0a344a7b689fa
JaeSeong Song <zzyzigy@naver.com>
2023-07-07 16:20:08 +0900

    우편번호 조회 : zipcode 맵핑 추가 (responsebody)

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/JoinController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import zzyzzy.hello.boot.spring5boot.model.Checkme;
 import zzyzzy.hello.boot.spring5boot.model.Member;
 import zzyzzy.hello.boot.spring5boot.service.MemberService;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/join")
@@ -75,5 +78,26 @@ public class JoinController {
 
         return "join/joinok";
     }
+
+
+    // 우편번호 검색
+    // /join/zipcode?dong=동이름
+    // /join/zipcode/동이름
+    // 검색결과는 뷰페이지 없이 바로 응답으로 출력 : RESTful 방식
+    // 서블릿에서 제공하는 HttpServletResponse 객체를 이용하면
+    // 스프링의 뷰리졸버 없이 바로 응답으로 출력할 수 있음
+    // 단, 응답유형은 JSON 형식으로 함
+    @GetMapping("/zipcode")
+    @ResponseBody
+    public void findzip(String dong, HttpServletResponse res) throws IOException {
+
+        // 우편번호 조회결과를 JSON 형식으로 보냄
+        // 따라서, 응답유형을 JSON형식으로 지정
+        res.setContentType("application/json; charset=utf-8");
+        // 검색된 결과를 뷰 없이 바로 응답response으로 출력
+        res.getWriter().print( msrv.findzip(dong) );
+
+    }
+
 
 }
