e06c302762b3a0f555a932e1ff3cc05f906b0afc
JaeSeong Song <zzyzigy@naver.com>
2023-07-10 15:50:05 +0900

    아이디 중복 검사 : checkuid 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -97,7 +97,7 @@ sendzip?.addEventListener('click', () => {
     }
 });
 
-email3.addEventListener('click', () => {
+email3?.addEventListener('click', () => {
     let frm = document.forms.joinfrm;
     if (email3.value === '직접입력하기') {
         frm.email2.readOnly = false;
@@ -113,5 +113,56 @@ dong?.addEventListener('keydown', (e) => {
     if (e.keyCode === 13)    // 엔터키(13)가 입력되면
         e.preventDefault();  // 이벤트 전파 방지
 });
+
+// 비밀번호 확인
+let pwd = document.joinfrm.passwd;
+let repwd = document.joinfrm.repasswd;
+let pwdmsg = document.querySelector("#pwdmsg");
+
+repwd?.addEventListener('blur', () => {
+    let pmsg = '비밀번호가 서로 일치하지 않습니다!!';
+    pwdmsg.className = 'text-danger';
+
+    if (pwd.value === repwd.value) {
+        pmsg = '비밀번호가 서로 일치합니다!!';
+        pwdmsg.className = 'text-primary';
+    }
+
+    pwdmsg.innerText = pmsg;
+});
+
+// 아이디 중복 검사
+let userid = document.joinfrm.userid;
+let checkuid = document.joinfrm.checkuid;
+let uidmsg = document.querySelector("#uidmsg");
+
+const styleCheckuid = (chkuid) => {
+    let umsg = '사용 불가능한 아이디입니다!!';
+    uidmsg.className = 'text-danger';
+    checkuid.value = 'no';
+
+    if (chkuid === '0') {
+        umsg = '사용 가능한 아이디입니다!!';
+        uidmsg.className = 'text-primary';
+        checkuid.value = 'yes';
+    }
+
+    uidmsg.innerText = umsg;
+};
+
+userid?.addEventListener('blur', () => {
+    if (userid.value === '') {
+        uidmsg.innerText = '6~16 자의 영문 소문자, 숫자와 특수기호(_)만 사용할 수 있습니다';
+        uidmsg.className = 'text-warning';
+        checkuid.value = 'no';
+        return;
+    }
+    const url = '/join/checkuid/' + userid.value;
+    fetch(url).then(response => response.text())
+        .then(text => styleCheckuid(text));
+});
+
+
+
 
 
