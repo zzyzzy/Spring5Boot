564e5ed7101b6de2807f8ff08558ca3530c69ce5
JaeSeong Song <zzyzigy@naver.com>
2023-07-10 18:13:31 +0900

    회원가입 : 주민번호 필드 추가

--- src/main/java/zzyzzy/hello/boot/spring5boot/model/Member.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/Member.java
@@ -12,6 +12,7 @@ public class Member {
     private String userid;
     private String passwd;
     private String name;
+    private String jumin;
     private String email;
     private String zipcode;
     private String addr1;
