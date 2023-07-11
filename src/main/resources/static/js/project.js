0f1c4790c33d1ed8c267bbe73afceaff580ffa25
JaeSeong Song <zzyzigy@naver.com>
2023-07-11 12:00:14 +0900

    로그인 : 로그인 폼 및 처리 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -203,3 +203,28 @@ let go2idx = document.querySelector("#go2idx");
 go2idx?.addEventListener('click', () => {
     location.href = '/';
 });
+
+// login
+let loginbtn = document.querySelector("#loginbtn");
+let lguid = document.querySelector("#userid");
+let lgpwd = document.querySelector("#passwd");
+let lgfrm = document.querySelector("#lgnfrm");
+
+loginbtn?.addEventListener('click', () => {
+   if (lguid.value === '') alert('아이디 입력하세요!!');
+   else if (lgpwd.value === '') alert('비밀번호 입력하세요!!');
+   else {
+        lgfrm.method = 'post';
+        lgfrm.action = '/join/login';
+        lgfrm.submit();
+   }
+});
+
+
+
+
+
+
+
+
+
