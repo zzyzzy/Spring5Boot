77c63a96426ea4f66840250044aeea07c212d83a
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 11:02:40 +0900

    게시판: BoardController list 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/BoardController.java
@@ -1,20 +1,28 @@
 package zzyzzy.hello.boot.spring5boot.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zzyzzy.hello.boot.spring5boot.service.BoardService;
 
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    final BoardService bsrv;
 
     Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m) {
         logger.info("board/list 호출!!");
+
+        m.addAttribute("bds", bsrv.readBoard());
 
         return "board/list";
     }
