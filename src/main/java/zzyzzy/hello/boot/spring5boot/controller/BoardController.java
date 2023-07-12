c84019862b738c0cadf0d2c00fcf5a02cd46df94
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 15:41:59 +0900

    게시판: BoardController view 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import zzyzzy.hello.boot.spring5boot.service.BoardService;
 
@@ -18,13 +19,23 @@ public class BoardController {
 
     Logger logger = LogManager.getLogger(BoardController.class);
 
-    @GetMapping("/list")
-    public String list(Model m, Integer cpg) {
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
         logger.info("board/list 호출!!");
 
         m.addAttribute("bds", bsrv.readBoard(cpg));
+        m.addAttribute("cpg", cpg);
 
         return "board/list";
+    }
+
+    @GetMapping("/view/{bno}")
+    public String view(Model m, @PathVariable String bno) {
+        logger.info("board/view 호출!!");
+
+        m.addAttribute("bd", bsrv.readOneBoard(bno));
+
+        return "board/view";
     }
 
 }
