c63e2ba7ecde76427c1f9e187a0e803f82119eea
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 10:12:54 +0900

    게시판 : board mapper 생성

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -0,0 +1,23 @@
+package zzyzzy.hello.boot.spring5boot.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+import zzyzzy.hello.boot.spring5boot.model.Board;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int insertBoard(Board b);
+
+    List<Board> selectBoard();
+
+    Board selectOneBoard(String bno);
+}
+
+
+
+
+
+
+
+
