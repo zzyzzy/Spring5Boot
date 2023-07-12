8ffed73922aae1baf3016f6405aca35ae1260fb2
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 11:03:09 +0900

    게시판: BoardController list 단위테스트

---
+++ src/test/java/zzyzzy/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -0,0 +1,40 @@
+package zzyzzy.hello.boot.spring5boot.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class BoardControllerUnitTest {
+    @Autowired private MockMvc mvc;
+
+    @Test
+    @DisplayName("BoardController read Test")
+    void readBoard() throws Exception {
+
+        mvc.perform(get("/board/list"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+}
+
+
+
+
+
+
+
