1caec465e605c4bc03bfc80e55805cfa486f370c
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 15:40:51 +0900

    게시판: BoardService 본문글 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/service/BoardServiceImpl.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/service/BoardServiceImpl.java
@@ -27,6 +27,8 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public Board readOneBoard(String bno) {
-        return null;
+
+        return bdao.selectOneBoard(bno);
     }
+
 }
