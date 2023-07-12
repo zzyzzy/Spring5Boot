1620d3e2165ee7e0540293b4f669e9f48322b7f3
JaeSeong Song <zzyzigy@naver.com>
2023-07-12 15:39:47 +0900

    게시판: boardDAO 본문글 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/dao/BoardDAOImpl.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/dao/BoardDAOImpl.java
@@ -27,6 +27,7 @@ public class BoardDAOImpl implements BoardDAO {
 
     @Override
     public Board selectOneBoard(String bno) {
-        return null;
+
+        return boardMapper.selectOneBoard(bno);
     }
 }
