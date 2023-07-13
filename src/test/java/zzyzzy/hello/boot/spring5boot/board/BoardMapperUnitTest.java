b7765d41012d50b99f2b4d1802a3d437150e1d05
JaeSeong Song <zzyzigy@naver.com>
2023-07-13 16:07:05 +0900

    게시판 검색 기능 mapper 단위테스트

--- src/test/java/zzyzzy/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/zzyzzy/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -9,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 import zzyzzy.hello.boot.spring5boot.model.Board;
 import zzyzzy.hello.boot.spring5boot.mybatis.BoardMapper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -71,6 +73,19 @@ public class BoardMapperUnitTest {
         int result = boardMapper.selectCountBoard();
 
         assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("boardMapper findBoard Test")
+    void findBoard() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("findtype", "titcont");
+        params.put("findkey", "의료");
+        params.put("stnum", 0);
+
+        List<Board> results = boardMapper.selectFindBoard(params);
+
+        assertNotNull(results);
     }
 
 
