42f9a8a6ea1ebe2a14ab67da2f5edef4f4204516
JaeSeong Song <zzyzigy@naver.com>
2023-07-14 16:05:36 +0900

    자료실 - PdsAttach 클래스 추가

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/PdsAttach.java
@@ -0,0 +1,19 @@
+package zzyzzy.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+
+    private String pano;
+    private String pno;
+    private String fname;
+    private String ftype;
+    private String fsize;
+    private String fdown;
+
+}
