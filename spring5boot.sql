9e2b904ec0f22855f974aca7f4adf85ad4db92df
JaeSeong Song <zzyzigy@naver.com>
2023-07-14 16:02:50 +0900

    자료실 - pds, pdsattach 테이블 추가

--- spring5boot.sql
+++ spring5boot.sql
@@ -66,5 +66,60 @@ select
     count(userid) cnt, ceil(count(userid) / 25) pages
 from board2;
 
+select * from board2
+where title like '%이강인%';
+
+-- pds
+create table pds (
+     pno         int             auto_increment,
+     title       varchar(100)    not null,
+     userid      varchar(18)     not null,
+     regdate     datetime        default current_timestamp,
+     thumbs      int             default 0,
+     views       int             default 0,
+     contents    text            not null,
+     ipaddr      varchar(15)     not null,
+     primary key (pno)
+);
+
+create table pdsattach (
+    pano        int             auto_increment,
+    pno         int             not null,  -- 게시글번호
+    fname       varchar(200)    not null,  -- uuid 포함
+    ftype       varchar(3)      not null,
+    fsize       float           not null,
+    fdown       int             default 0,
+    primary key (pano)
+);
+
+alter table pds
+    add constraint fkpuid
+        foreign key (userid) references member2 (userid);
+
+alter table pdsattach
+    add constraint fkpno
+        foreign key (pno) references pds (pno);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
