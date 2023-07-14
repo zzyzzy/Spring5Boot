c09d6540a82e62346826b9868832e1c71583015c
JaeSeong Song <zzyzigy@naver.com>
2023-07-14 18:05:45 +0900

    자료실 - PdsDAOImpl : insertPdsAttach 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/dao/PdsDAO.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/dao/PdsDAO.java
@@ -1,9 +1,12 @@
 package zzyzzy.hello.boot.spring5boot.dao;
 
 import zzyzzy.hello.boot.spring5boot.model.Pds;
+import zzyzzy.hello.boot.spring5boot.model.PdsAttach;
 
 public interface PdsDAO {
 
     int insertPds(Pds p);
+
+    int insertPdsAttach(PdsAttach pa);
 
 }
--- src/main/java/zzyzzy/hello/boot/spring5boot/dao/PdsDAOImpl.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/dao/PdsDAOImpl.java
@@ -3,6 +3,7 @@ package zzyzzy.hello.boot.spring5boot.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import zzyzzy.hello.boot.spring5boot.model.Pds;
+import zzyzzy.hello.boot.spring5boot.model.PdsAttach;
 import zzyzzy.hello.boot.spring5boot.mybatis.PdsMapper;
 
 @Repository("pdao")
@@ -19,6 +20,12 @@ public class PdsDAOImpl implements PdsDAO {
             cnt = pdsMapper.lastPdsPno();
 
         return cnt;
+    }
+
+    @Override
+    public int insertPdsAttach(PdsAttach pa) {
+
+        return pdsMapper.insertPdsAttach(pa);
     }
 
 }
