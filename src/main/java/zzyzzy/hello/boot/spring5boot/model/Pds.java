8e03caf6d70b8fe051ea1cf32557d169f6b911c9
JaeSeong Song <zzyzigy@naver.com>
2023-07-14 16:05:01 +0900

    자료실 - Pds 클래스 추가

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/model/Pds.java
@@ -0,0 +1,21 @@
+package zzyzzy.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pds {
+
+    private String pno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+
+}
