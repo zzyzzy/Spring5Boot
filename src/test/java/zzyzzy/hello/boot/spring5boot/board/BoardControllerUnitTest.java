6225513d61a45cb62e965d5ec8d30d5b399efa3d
JaeSeong Song <zzyzigy@naver.com>
2023-07-14 11:05:55 +0900

    게시판 검색 건수 BoardController 단위테스트

--- src/test/java/zzyzzy/hello/boot/spring5boot/board/BoardControllerUnitTest.java
+++ src/test/java/zzyzzy/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -57,7 +57,7 @@ public class BoardControllerUnitTest {
     @DisplayName("BoardController find Test")
     void findBoard() throws Exception {
 
-        mvc.perform(get("/board/find/1/title/이강인"))
+        mvc.perform(get("/board/find/title/이강인/1"))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
