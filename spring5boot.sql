eeac8146061f457b98602d7f46f8d126d34f3c7d
JaeSeong Song <zzyzigy@naver.com>
2023-07-17 13:42:33 +0900

    자료실 - Pds, PdsAttach join 및 view 추가

--- spring5boot.sql
+++ spring5boot.sql
@@ -100,7 +100,17 @@ alter table pdsattach
     add constraint fkpno
         foreign key (pno) references pds (pno);
 
+-- join
+select * from pds p join pdsattach pa
+                         using(pno) where p.pno = '5';
 
+-- view (가상테이블)
+create view ppa
+as
+select * from pds p join pdsattach pa
+             using(pno);
+
+select * from ppa where pno = '5';
 
 
 
