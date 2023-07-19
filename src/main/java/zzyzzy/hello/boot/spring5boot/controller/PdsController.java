9885188882ba66eec997859083f590a9d8cad33a
JaeSeong Song <zzyzigy@naver.com>
2023-07-19 14:03:34 +0900

    댓글 - cmtwrite, replywrite 구현, view에서 댓글/대댓글 출력

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/PdsController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import zzyzzy.hello.boot.spring5boot.model.Board;
 import zzyzzy.hello.boot.spring5boot.model.Pds;
+import zzyzzy.hello.boot.spring5boot.model.PdsComment;
 import zzyzzy.hello.boot.spring5boot.service.PdsService;
 
 import java.util.Map;
@@ -75,7 +76,8 @@ public class PdsController {
     public String view(Model m, @PathVariable String pno) {
         logger.info("pds/view 호출!!");
 
-        m.addAttribute("p", psrv.readOnePds(pno));
+        m.addAttribute("p", psrv.readOnePds(pno));  // 본문글
+        m.addAttribute("pcs", psrv.readPdsComment(pno)); // 댓글, 대댓글
 
         return "pds/view";
     }
@@ -96,5 +98,35 @@ public class PdsController {
                 .body((UrlResource)objs.get("resource"));
     }
 
+
+    @PostMapping("/cmt/write")
+    public String cmtwriteok(PdsComment pc) {
+        logger.info("pds/cmt/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 댓글을 테이블에 저장
+        if (psrv.newPdsComment(pc)) {
+            // 작성한 댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
+    }
+
+    @PostMapping("/reply/write")
+    public String rpywriteok(PdsComment pc) {
+        logger.info("pds/reply/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 대댓글을 테이블에 저장
+        if (psrv.newPdsReply(pc)) {
+            // 작성한 대댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
+    }
 
 }
