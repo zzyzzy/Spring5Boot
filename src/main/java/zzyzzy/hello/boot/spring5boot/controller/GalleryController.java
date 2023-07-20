435ff165286600a09140b4aeaff6b7d65341f9c4
JaeSeong Song <zzyzigy@naver.com>
2023-07-20 11:20:01 +0900

    갤러리 :  GalleryController list 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/GalleryController.java
@@ -1,20 +1,35 @@
 package zzyzzy.hello.boot.spring5boot.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zzyzzy.hello.boot.spring5boot.service.GalleryService;
 
 @Controller
 @RequestMapping("/gallery")
+@RequiredArgsConstructor
 public class GalleryController {
+
+    final GalleryService gsrv;
 
     Logger logger = LogManager.getLogger(GalleryController.class);
 
-    @GetMapping("/list")
-    public String list() {
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
         logger.info("gallery/list 호출!!");
+
+        m.addAttribute("gal", gsrv.readGallery(cpg));
+        m.addAttribute("cpg", cpg);
+        m.addAttribute("cntpg", gsrv.countGallery());
+        m.addAttribute("stpg", ((cpg - 1) / 10) * 10 + 1);
+
+        if (cpg > (int)m.getAttribute("cntpg"))
+            return "redirect:/gallery/list/1";
 
         return "gallery/list";
     }
