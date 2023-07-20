a16dfbbb97f4654f9890cc063509ff158f3512fd
JaeSeong Song <zzyzigy@naver.com>
2023-07-20 11:18:35 +0900

    갤러리 :  Gallery 매퍼 추가

---
+++ src/main/java/zzyzzy/hello/boot/spring5boot/mybatis/GalleryMapper.java
@@ -0,0 +1,40 @@
+package zzyzzy.hello.boot.spring5boot.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface GalleryMapper {
+    int insertGallery(Gallery g);
+
+    int insertGalAttach(GalAttach ga);
+
+    List<Gallery> selectGallery(int stnum);
+
+    Gallery selectOneGallery(String gno);
+
+    int selectCountGallery();
+
+    GalAttach selectOneGalAttach(String gno);
+
+    /*int insertPdsComment(PdsComment pc);
+
+    List<PdsComment> selectPdsComment(String pno);
+
+    int insertPdsReply(PdsComment pc);
+
+    int updateViewPds(String bno);
+
+    List<Pds> selectFindPds(Map<String, Object> params);
+
+    int countFindPds(Map<String, Object> params);*/
+}
+
+
+
+
+
+
+
+
