8fbb2cad21fd36d2c2195bbc098a91d916a31890
JaeSeong Song <zzyzigy@naver.com>
2023-07-21 10:48:54 +0900

    갤러리 : insertGallery, insertGalAttach 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/dao/GalleryDAO.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/dao/GalleryDAO.java
@@ -1,7 +1,14 @@
 package zzyzzy.hello.boot.spring5boot.dao;
 
+import zzyzzy.hello.boot.spring5boot.model.GalAttach;
+import zzyzzy.hello.boot.spring5boot.model.Gallery;
+
 import java.util.List;
 
 public interface GalleryDAO {
     List<Gallery> selectGallery(int stnum);
+
+    int insertGallery(Gallery g);
+
+    int insertGalAttach(GalAttach ga);
 }
--- src/main/java/zzyzzy/hello/boot/spring5boot/dao/GalleryDAOImpl.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/dao/GalleryDAOImpl.java
@@ -2,6 +2,8 @@ package zzyzzy.hello.boot.spring5boot.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import zzyzzy.hello.boot.spring5boot.model.GalAttach;
+import zzyzzy.hello.boot.spring5boot.model.Gallery;
 import zzyzzy.hello.boot.spring5boot.mybatis.GalleryMapper;
 
 import java.util.List;
@@ -16,5 +18,19 @@ public class GalleryDAOImpl implements GalleryDAO {
     public List<Gallery> selectGallery(int stnum) {
 
         return galleryMapper.selectGallery(stnum);
+    }
+
+    @Override
+    public int insertGallery(Gallery g) {
+        int cnt = galleryMapper.insertGallery(g);
+        if (cnt > 0) cnt = galleryMapper.lastGalGno();
+
+        return cnt;
+    }
+
+    @Override
+    public int insertGalAttach(GalAttach ga) {
+
+        return galleryMapper.insertGalAttach(ga);
     }
 }
