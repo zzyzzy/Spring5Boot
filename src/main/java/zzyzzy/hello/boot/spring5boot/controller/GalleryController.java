a921bf450b2e0266b16f1a5201219c67867fbfc7
JaeSeong Song <zzyzigy@naver.com>
2023-07-21 10:52:57 +0900

    갤러리 : write, writeok 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/controller/GalleryController.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import zzyzzy.hello.boot.spring5boot.model.Gallery;
+import zzyzzy.hello.boot.spring5boot.model.Pds;
 import zzyzzy.hello.boot.spring5boot.service.GalleryService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/gallery")
@@ -28,10 +34,37 @@ public class GalleryController {
         m.addAttribute("cntpg", gsrv.countGallery());
         m.addAttribute("stpg", ((cpg - 1) / 10) * 10 + 1);
 
-        if (cpg > (int)m.getAttribute("cntpg"))
-            return "redirect:/gallery/list/1";
+        //if (cpg > (int)m.getAttribute("cntpg"))
+        //    return "redirect:/gallery/list/1";
 
         return "gallery/list";
     }
+
+    @GetMapping("/write")
+    public String write() {
+        logger.info("gallery/write 호출!!");
+
+        return "gallery/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Gallery g, List<MultipartFile> attachs) {
+        logger.info("gallery/writeok 호출!!");
+
+        String returnPage = "redirect:/gallery/fail";
+
+        // 작성한 게시글을 먼저 디비에 저장하고 글번호를 알아냄
+        int gno = gsrv.newGallery(g);
+
+        // 알아낸 글번호를 이용해서 첨부파일 처리 (디비저장, 업로드)
+        if (!attachs.isEmpty()) {// 첨부파일이 존재한다면
+            gsrv.newGalAttach(attachs, gno);
+
+            returnPage = "redirect:/gallery/list/1";
+        }
+
+        return returnPage;
+    }
+
 
 }
