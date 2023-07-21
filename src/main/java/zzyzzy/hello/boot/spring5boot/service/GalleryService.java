d886a54269ac6226f61ac37bb4344ed514b0f2a9
JaeSeong Song <zzyzigy@naver.com>
2023-07-21 10:49:37 +0900

    갤러리 : newGallery, newGalAttach 구현

--- src/main/java/zzyzzy/hello/boot/spring5boot/service/GalleryService.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/service/GalleryService.java
@@ -1,9 +1,16 @@
 package zzyzzy.hello.boot.spring5boot.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import zzyzzy.hello.boot.spring5boot.model.Gallery;
+
 import java.util.List;
 
 public interface GalleryService {
     List<Gallery> readGallery(Integer cpg);
 
     int countGallery();
+
+    int newGallery(Gallery g);
+
+    boolean newGalAttach(List<MultipartFile> attachs, int gno);
 }
--- src/main/java/zzyzzy/hello/boot/spring5boot/service/GalleryServiceImpl.java
+++ src/main/java/zzyzzy/hello/boot/spring5boot/service/GalleryServiceImpl.java
@@ -2,7 +2,11 @@ package zzyzzy.hello.boot.spring5boot.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import zzyzzy.hello.boot.spring5boot.dao.GalleryDAO;
+import zzyzzy.hello.boot.spring5boot.model.GalAttach;
+import zzyzzy.hello.boot.spring5boot.model.Gallery;
+import zzyzzy.hello.boot.spring5boot.utils.GalleryUtils;
 
 import java.util.List;
 
@@ -11,6 +15,8 @@ import java.util.List;
 public class GalleryServiceImpl implements GalleryService {
 
     final GalleryDAO gdao;
+    final GalleryUtils galUtils;
+
 
     @Override
     public List<Gallery> readGallery(Integer cpg) {
@@ -21,5 +27,25 @@ public class GalleryServiceImpl implements GalleryService {
     @Override
     public int countGallery() {
         return 0;
+    }
+
+    @Override
+    public int newGallery(Gallery g) {
+
+        return gdao.insertGallery(g);
+    }
+
+    @Override
+    public boolean newGalAttach(List<MultipartFile> attachs, int gno) {
+        // 이미지 파일 저장후 파일 정보(들) 받아오기
+        GalAttach ga = galUtils.processUpload(attachs);
+
+        // 썸내일 이미지 생성
+        galUtils.makeThumbnail(ga);
+
+        // 이미지 파일 정보 저장
+        ga.setGno(gno + "");
+
+        return (gdao.insertGalAttach(ga) > 0) ? true : false;
     }
 }
